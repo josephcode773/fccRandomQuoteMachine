46de1187ab3af2ac74b94919f2ec7b1e81e7929f
josephcode773 <josephcode773@gmail.com>
2016-11-27 00:04:49 -0600

    working on "tweet this" button

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $("#newQuote").on("click", function () {
-        $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function (json) {
-            $("#message").html(JSON.stringify(json));
-        });
-    });
-
-});
